f7ff5f38711a3c8dd438825774755d7fcf695929
Silvia Luschi <s.luschi@reply.it>
2019-07-12 12:39:59 +0200

    estrapolare valori da mappa

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,9 @@
 package com.company;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collector;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Main {
 
@@ -21,7 +22,14 @@ public class Main {
                 "Lost for Words",
                 "High Hopes"));
         Main m=new Main();
-        m.autorePrimaAnno(albums, 2000);
+        //m.autorePrimaAnno(albums, 2000);
+
+        Map< String, List < String >> phoneNumbers = new HashMap< String, List < String >>();
+        phoneNumbers.put("John Lawson", Arrays.asList("3232312323", "8933555472"));
+        phoneNumbers.put("Mary Jane", Arrays.asList("12323344", "492648333"));
+        phoneNumbers.put("Mary Lou", Arrays.asList("77323344", "938448333"));
+
+        m.numeriTelefono(phoneNumbers);
     }
 
     public void autorePrimaAnno(List<Album> album, int anno){
@@ -36,5 +44,14 @@ public class Main {
         StringBuilder stringBuilder=new StringBuilder();
         String nomeCognome=stringBuilder.append("sILVIA").append(" ").append("LUSCHI").toString();
         System.out.println(nomeCognome);
+    }
+
+    public void numeriTelefono(Map< String, List < String >> phoneNumbers){
+
+        ArrayList<String> chiavi = phoneNumbers.keySet().stream().collect(Collectors.toCollection(ArrayList::new));
+        chiavi.forEach(System.out::println);
+        List<String> lista = phoneNumbers.entrySet().stream().filter(k -> k.getKey().equals("Mary Jane")).map(Map.Entry::getValue).flatMap(List::stream).collect(Collectors.toList());
+        lista.stream().forEach(System.out::println);
+
     }
 }
